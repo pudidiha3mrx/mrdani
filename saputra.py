b14f723f35b360ad965a744181c5bf590a4f2e1b
pudidiha3mrx <91170250+pudidiha3mrx@users.noreply.github.com>
2021-10-14 09:59:50 +0700

    Update saputra.py

--- saputra.py
+++ saputra.py
@@ -3,8 +3,6 @@
 import socket, random, time, os, sys
 
 if len(sys.argv) < 5:
-os.system("clear")
-print(" God UDP Flood MR.DANI ")
         sys.exit("Usage: python "+sys.argv[0]+" <ip> <port> <size> <time>")
 
 ip = sys.argv[1]
